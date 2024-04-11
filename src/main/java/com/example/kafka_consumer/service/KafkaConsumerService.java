6e8f93ed91d7f24d7beaa5819338c4ded7be51a8
Eunbin Bae <81680796+eungbean1029@users.noreply.github.com>
2024-04-11 22:35:24 +0900

    kafka 컨슈머 서비스 예제코드

--- src/main/java/com/example/kafka_consumer/service/KafkaConsumerService.java
+++ src/main/java/com/example/kafka_consumer/service/KafkaConsumerService.java
@@ -1,4 +1,16 @@
 package com.example.kafka_consumer.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
 public class KafkaConsumerService {
+    @KafkaListener(topics = "my-topic", groupId = "consumer_group01")
+    public void consume(String message) throws IOException {
+        System.out.printf("Consumed Message : %s%n", message);
+    }
 }
